9f0b1b2fcdfebbbd044c2ae26f42d9e0d3a19a58
Chris Gullickson <cgullickson5@gmail.com>
2017-12-11 17:24:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,7 @@ class Triangle
 
   def kind
     if @sides.any? {|side| side <= 0}
-      
-
-
+      raise TriangleError
     elsif  @side_a == @side_b && @side_b == @side_c
       :equilateral
     elsif @side_a == @side_b || @side_b == @side_c || @side_c == @side_a
